53917e0bd0a52cdba195eb74cd1a38cfeda63004
Charlene Han <45677438+Charlmzz@users.noreply.github.com>
2022-12-03 04:37:52 +0000

    add comments

--- create_db.py
+++ create_db.py
@@ -1,16 +1,20 @@
 import sqlite3
 import pandas as pd
 
+#read csv
 data = pd.read_csv(r'WorldCups.csv')
 df = pd.DataFrame(data)
 
+#set up database connection
 conn = None
 conn = sqlite3.connect("worldcup.db")
 cursor = conn.cursor()
 
+#drop existing world_cup table
 DROP_TABLE = "DROP TABLE world_cup"
 cursor.execute(DROP_TABLE)
 
+#create new world_cup table
 CREATE_TABLE = """ CREATE TABLE world_cup (
                 year integer PRIMARY KEY, 
                 country text NOT NULL,
@@ -26,8 +30,10 @@ CREATE_TABLE = """ CREATE TABLE world_cup (
 cursor.execute(CREATE_TABLE)
 conn.commit()
 
+#add data to the table
 df.to_sql('world_cup',conn, if_exists='replace', index=False)
 
+#print out all data in the table
 cursor.execute("SELECT * FROM world_cup")
 print(cursor.fetchall())
 
